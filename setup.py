77b155da0e1c13629423089389cb9b33619c5673
WAKAYAMA Shirou <shirou.faw@gmail.com>
2012-08-23 23:30:24 +0900

    version 0.0.4

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ classifiers = [
 
 setup(
     name='dipus',
-    version="0.0.3",
+    version="0.0.4",
     description='Dipus is a simple full-text search server using Whoosh for Sphinx',
     long_description=open("README.rst").read(),
     classifiers=classifiers,
